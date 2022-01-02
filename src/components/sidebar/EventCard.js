e22da76467ac54f2d6e7c08450f6ed9368b2d14b
tnngoan <ngoan.n.tr@gmail.com>
2022-01-03 05:17:15 +0700

    update data set

--- src/components/sidebar/EventCard.js
+++ src/components/sidebar/EventCard.js
@@ -1,10 +1,11 @@
 import React from "react";
+import dayjs from "dayjs";
 
 function EventCard(props) {
   return (
-    <div className="h-12">
+    <div className="h-auto p-2 border border-gray-300 rounded-lg m-2">
       <h3 className="text-ellipsis truncate overflow-hidden">{props.title}</h3>
-      <p>{props.time}</p>
+      <p>{dayjs(props.date).format("DD-MM-YYYY")}</p>
     </div>
   );
 }
--- src/components/sidebar/SidebarEvents.js
+++ src/components/sidebar/SidebarEvents.js
@@ -16,12 +16,12 @@ function SidebarEvents() {
 
   function getAllEvents() {
     let futureEvents = data.filter((event) => {
-      return dayjs().isAfter(dayjs(event.event_time)) === false;
+      return dayjs().isAfter(dayjs(event.date)) === false;
     });
 
     futureEvents = futureEvents.sort(function (a, b) {
-      a = dayjs(a.event_time);
-      b = dayjs(b.event_time);
+      a = dayjs(a.date);
+      b = dayjs(b.date);
       return a.diff(today) - b.diff(today);
     });
 
@@ -30,13 +30,13 @@ function SidebarEvents() {
 
   const getTodayEvents = async () => {
     const todayEvents = data.filter((event) => {
-      return dayjs(event.event_time).format(formatForm) === today;
+      return dayjs(event.date).format(formatForm) === today;
     });
     setEvents(todayEvents);
   };
 
   return (
-    <div className="px-4 bg-red-200">
+    <div className="px-4">
       <div className="flex align-baseline justify-between">
         <h3 className="font-bold text-blue-800 text-xl">Upcoming Events</h3>
         <button
@@ -50,11 +50,7 @@ function SidebarEvents() {
         <p className="text-md">Today, 1 Jan</p>
       </div>
       {events.map((event) => (
-        <EventCard
-          key={event.id}
-          title={event.event_title}
-          time={event.event_time}
-        />
+        <EventCard key={event.id} title={event.name} date={event.date} />
       ))}
       {console.log(events)}
     </div>
